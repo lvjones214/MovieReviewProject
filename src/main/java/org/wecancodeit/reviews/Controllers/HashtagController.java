d53f86ddc76d3c8db0da7436a253446238fff979
tainaoual <Tai Naoual>
2020-10-16 15:02:05 -0400

    add hashtags work

--- src/main/java/org/wecancodeit/reviews/Controllers/HashtagController.java
+++ src/main/java/org/wecancodeit/reviews/Controllers/HashtagController.java
@@ -2,19 +2,35 @@ package org.wecancodeit.reviews.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.wecancodeit.reviews.Hashtag;
 import org.wecancodeit.reviews.storage.HashtagStorage;
+import org.wecancodeit.reviews.storage.MovieStorage;
 
 @Controller
 public class HashtagController {
     private HashtagStorage hashtagStorage;
-    public HashtagController(HashtagStorage hashtagStorage) {
+    private MovieStorage movieStorage;
+    public HashtagController(HashtagStorage hashtagStorage, MovieStorage movieStorage) {
         this.hashtagStorage = hashtagStorage;
+        this.movieStorage = movieStorage;
     }
+
+
     @RequestMapping("/Hashtag/{id}")
     public String displayHashtags(Model model, @PathVariable long id) {
         model.addAttribute("hashtags", hashtagStorage.retrieveHashtagById(id));
 return "Hashtagtemplate";
     }
+  @PostMapping("/hashtag")
+
+  public String addHashtag(@RequestParam String hashtagName ,@RequestParam long reviewId ){
+       Hashtag hashtagToAdd = new Hashtag(hashtagName, movieStorage.retrieveMovieReviewById(reviewId));
+      hashtagStorage.addHashtag(hashtagToAdd);
+
+      return "redirect:/reviews/"+reviewId;
+
+  }
+
 }
+
--- src/main/java/org/wecancodeit/reviews/Controllers/MovieReviewController.java
+++ src/main/java/org/wecancodeit/reviews/Controllers/MovieReviewController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.wecancodeit.reviews.Models.MovieGenre;
@@ -27,7 +28,9 @@ public MovieReviewController(MovieStorage movieStorage) {
 //
 //        return "reviewtemplates";
 //    }
-    @RequestMapping("/reviews/{id}")
+
+    @GetMapping("/reviews/{id}")
+
     public String showMovieReview(Model model, @PathVariable long id){
 //        log.info("The id is: " + id);
 //        log.info(movieStorage.retrieveMovieReviewById(id).toString());
