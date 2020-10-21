f2badd44cd5ba344a5260b4716c952daa621f6de
unknown <michie_tiff@hotmail.com>
2020-10-21 11:02:40 -0400

    review forms html created

--- src/main/java/org/wecancodeit/reviews/Controllers/MovieReviewController.java
+++ src/main/java/org/wecancodeit/reviews/Controllers/MovieReviewController.java
@@ -5,10 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.wecancodeit.reviews.Hashtag;
 import org.wecancodeit.reviews.Models.MovieGenre;
+import org.wecancodeit.reviews.Models.MovieReview;
 import org.wecancodeit.reviews.storage.MovieStorage;
 
 @Controller
@@ -36,6 +36,14 @@ public MovieReviewController(MovieStorage movieStorage) {
 //        log.info(movieStorage.retrieveMovieReviewById(id).toString());
         model.addAttribute("movieReview", movieStorage.retrieveMovieReviewById(id));  
         return "reviewtemplates";
+    }
+    @PostMapping("/moviereview")
+    public String addMovieReview(@RequestParam String movieName, @RequestParam String genreName, @RequestParam int yearReleased,@RequestParam String mpaaRating,@RequestParam String starRating,@RequestParam String hashtagName,@RequestParam String imgUrlName,@RequestParam String movieDescriptionName,@RequestParam String movieReviewName){
+        MovieReview movieReviewToAdd = new MovieReview(movieName, genreName, yearReleased, mpaaRating, starRating, movieDescriptionName, movieReviewName);
+   // should be genre object for genre name
+        // figure out how to add img URL and hashtag to movie review object.
+        hashtagStorage.addHashtag(hashtagToAdd);
+        return "redirect:/reviews/"+reviewId;
     }
 
 }
